3fab5909eee209fabaa2f568027230cbd0cc3058
tarkhand <Tarun.Khandelwal1@amdocs.com>
2019-08-09 10:26:49 +0530

    Added Cross-Origin in Repository, so it can be consume by other application;Added new model class;Fixed bugs

--- newspaper-management-system/src/main/java/com/newspaper/repository/UnitPriceRepository.java
+++ newspaper-management-system/src/main/java/com/newspaper/repository/UnitPriceRepository.java
@@ -4,6 +4,9 @@
 package com.newspaper.repository;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.newspaper.model.UnitPrice;
 
@@ -11,6 +14,9 @@ import com.newspaper.model.UnitPrice;
  * @author tarkhand
  *
  */
+@RepositoryRestResource(collectionResourceRel = "unit-price", path = "unit-price", exported = true)
+@CrossOrigin(allowCredentials = "false", allowedHeaders = "*", methods = { RequestMethod.POST, RequestMethod.GET,
+		RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.PATCH })
 public interface UnitPriceRepository extends PagingAndSortingRepository<UnitPrice, Long> {
 
 }
