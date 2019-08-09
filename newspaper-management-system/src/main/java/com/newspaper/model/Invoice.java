ea69d23e75e55464f81e604ce7b08680b4a8e073
nisargkp <nsrg.ptl@gmail.com>
2019-08-09 11:24:09 +0530

    remove unused import

--- newspaper-management-system/src/main/java/com/newspaper/model/Invoice.java
+++ newspaper-management-system/src/main/java/com/newspaper/model/Invoice.java
@@ -4,7 +4,6 @@
 package com.newspaper.model;
 
 import java.util.Date;
-import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -12,7 +11,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
